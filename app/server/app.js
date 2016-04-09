840b9107d51761ef1e618e8cbb20e5f0f2903911
Metux <mete.selcuk@clicrdv.com>
2016-04-09 18:12:17 +0200

    localhost with index.html

--- app/server/app.js
+++ app/server/app.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var app = express();
 
+
+app.use(express.static('public'));
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  console.log(req, res);
+  res.sendFile('index.html');
+});
+app.get('/toto', function (req, res) {
+  res.send('Hello toto!');
+});
+app.get('/tutu', function (req, res) {
+  res.send('Hello tutu!');
 });
 
 app.listen(2000, function () {
